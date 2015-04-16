aa135538a499a15fdf9e388a7fd61fbd130cdbed
BenPowley <powleybenj@ecs.vuw.ac.nz>
2015-04-17 11:42:01 +1200

    add code for setting up simulator class

--- src/modelrailway/simulation/Section.java
+++ src/modelrailway/simulation/Section.java
@@ -9,29 +9,43 @@ import java.util.Set;
  * @author powleybenj
  *
  */
-public class Section extends ArrayList<Track>{ // a section is a list of tracks , sections can detect trains 
+public class Section extends ArrayList<Track>{ // a section is a list of tracks , sections can detect trains
+
+
+	static{
+		sectionNumberCounter = 0;
+	}
 
 	private Set<Movable> movableObjects = new HashSet<Movable>(); // the trains on the section.
+
 	/**
 	 * Section takes a list of tracks in the section.
 	 * @param tr
 	 */
+	private static int sectionNumberCounter;
+	private int sectionNumber;
 	public Section(List<Track> tr){
+		sectionNumber = sectionNumberCounter;
+		sectionNumberCounter++;
 		this.addAll(tr);
 	}
-	
+
+	public int getNumber(){
+		return sectionNumber;
+	}
+
 	public boolean addMovable(Movable m){
 		return movableObjects.add(m);
 	}
-	
+
 	public boolean containsMovable(Movable m){
 		return movableObjects.contains(m);
 	}
-	
+
 	public boolean removeMovable(Movable m){
 		return movableObjects.remove(m);
 	}
-	
+
 	public boolean emptyMovable(){
 		return (movableObjects.size() == 0);
 	}
--- src/modelrailway/simulation/Simulator.java
+++ src/modelrailway/simulation/Simulator.java
@@ -1,18 +1,57 @@
 package modelrailway.simulation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import modelrailway.core.Controller;
 import modelrailway.core.Event;
 import modelrailway.core.Event.Listener;
 import modelrailway.core.Route;
 import modelrailway.core.Train;
 /**
- * 
+ *
  * @author powleybenj
  *
  */
 public class Simulator implements Controller{
 
-	public Simulator(Track track){
+
+	private class TrainThread extends Thread{
+
+		private Map<Integer, modelrailway.simulation.Train> modelTrains;
+		private Map<Integer, Route> trainRoute;
+
+		public TrainThread(Map<Integer, modelrailway.simulation.Train> modelTrains, Map<Integer, Train> trains){
+			this.modelTrains = modelTrains;
+		}
+
+		public void run(){
+
+		}
+
+		public boolean startTrain(int trainID, Route route){
+			if(modelTrains.containsKey(trainID)){
+			    modelTrains.get(trainID).stop();
+			    return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		public void stopTrain(int trainID){
+			if(modelTrains.containsKey(trainID)) modelTrains.get(trainID);
+		}
+	}
+	private ArrayList<Listener> listeners = new ArrayList<Listener>();
+	private Map<Integer,Train> trainMap; // a map from train id's to trains.
+	private TrainThread runningThread;
+
+
+	public Simulator(Track track, Map<Integer,Train> map, Map<Integer,modelrailway.simulation.Train> trains){
+		trainMap = map;
+		runningThread = new TrainThread(trains,map);
 
 	}
 	@Override
@@ -23,26 +62,25 @@ public class Simulator implements Controller{
 
 	@Override
 	public void register(Listener listener) {
-		// TODO Auto-generated method stub
+		listeners.add(listener);
 
 	}
 
 	@Override
 	public boolean start(int trainID, Route route) {
-		// TODO Auto-generated method stub
-		return false;
+		return runningThread.startTrain(trainID,route);
 	}
 
 	@Override
 	public void stop(int trainID) {
-		// TODO Auto-generated method stub
+		runningThread.stopTrain(trainID);
 
 	}
 
 	@Override
 	public Train train(int trainID) {
 		// TODO Auto-generated method stub
-		return null;
+		return trainMap.get(trainID);
 	}
 
 }
