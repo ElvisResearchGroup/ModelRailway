0552332203bff99f093c4bda5e3abf32e5c622de
BenPowley <powleybenj@ecs.vuw.ac.nz>
2015-04-24 12:50:07 +1200

    The Section class has been modified so that the calling method is responsible for stopping and starting the trains to preserve encapsulation

--- src/modelrailway/simulation/Section.java
+++ src/modelrailway/simulation/Section.java
@@ -15,7 +15,11 @@ import modelrailway.core.Controller;
  *
  */
 public class Section extends ArrayList<Track>{ // a section is a list of tracks , sections can detect trains
-
+	public class RemovePair{
+		public RemovePair(boolean retValue, Train listedTrain){this.retValue = retValue; this.listedTrain = listedTrain;}
+		public boolean retValue;
+		public Train listedTrain;
+	}
 
 	static{
 		sectionNumberCounter = 0;
@@ -24,10 +28,6 @@ public class Section extends ArrayList<Track>{ // a section is a list of tracks 
 	private Set<Movable> movableObjects = new HashSet<Movable>(); // the trains on the section.
 
 	private Queue<Train> entryRequests = new LinkedList<Train>();
-
-	private Controller ctl = null;
-
-	private Map<Integer,Train> trains;
 
 	/**
 	 * Section takes a list of tracks in the section.
@@ -40,17 +40,15 @@ public class Section extends ArrayList<Track>{ // a section is a list of tracks 
 		sectionNumberCounter++;
 		this.addAll(tr);
 	}
-
-	public Section(List<Track> tr, Controller ctl, Map<Integer,Train> trains){
-		 this.ctl = ctl;
-		 this.trains = trains;
-		 sectionNumber = sectionNumberCounter;
-		 sectionNumberCounter ++;
-		 this.addAll(tr);
-	}
-
+	/**
+	 * The reserveSection method reserves the section for the supplied train. if there is nothing on the waiting list the section is reserved
+	 * and true is returned. otherwise false is returned. The calling method should stop the train if false is returned.
+	 * @param t
+	 * @return
+	 */
 	public boolean reserveSection(Train t){
 		if(entryRequests.size() == 0 && entryRequests.size() == 0){
+			entryRequests.offer(t);
 			return true;
 		}
 		else {
@@ -77,19 +75,20 @@ public class Section extends ArrayList<Track>{ // a section is a list of tracks 
 		return movableObjects.contains(m);
 	}
 
-	public boolean removeMovable(Movable m){
+	/**
+	 * The removeMovable method removes a movable object from the list of Movable objects in the Section,
+	 * then the method returns a pair containing the return value of the removal and a train on the waiting queue if there is one.
+	 * The calling method is responsible for notifying the train that it may start.
+	 * @param m
+	 * @return
+	 */
+	public Section.RemovePair removeMovable(Movable m){
 		boolean ret = movableObjects.remove(m);
-		if(ctl != null){ // else dont send the notification.
+		Train tr = null;
 		if(movableObjects.size() == 0){
-			Train tr = entryRequests.peek();
-			Integer i = null;
-			for(Map.Entry<Integer, Train> ent : trains.entrySet()){
-				if(ent.getValue() == tr) i = ent.getKey(); // find the id of the train from the supplied list of trains.
-			}
-			tr.notifySectionFree(i,ctl);
+		    tr = entryRequests.peek();
 		}
-		}
-		return ret;
+		return new Section.RemovePair(ret,tr);
 	}
 
 	public boolean emptyMovable(){
