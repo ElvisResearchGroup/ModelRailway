99d56ba77cebe3466cdf1c6846f6aa64ebde74c5
BenPowley <powleybenj@ecs.vuw.ac.nz>
2015-05-15 10:00:17 +1200

    Insert test for ModelTrack

---
+++ src/modelrailway/testing/ModelTrackTest.java
@@ -0,0 +1,123 @@
+package modelrailway.testing;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+import org.junit.Test;
+
+import modelrailway.core.Controller;
+import modelrailway.core.Event;
+import modelrailway.core.Section;
+import modelrailway.core.Event.Listener;
+import modelrailway.core.Event.SectionChanged;
+import modelrailway.core.Route;
+import modelrailway.simulation.BackSwitch;
+import modelrailway.simulation.ForwardSwitch;
+import modelrailway.simulation.Straight;
+import modelrailway.simulation.Track;
+import modelrailway.simulation.Locomotive;
+import modelrailway.simulation.Movable;
+import modelrailway.simulation.Train;
+import modelrailway.simulation.Simulator;
+import modelrailway.util.Pair;
+
+import java.util.HashMap;
+
+import org.junit.*;
+
+public class ModelTrackTest {
+
+	@Test public void modelTrackTest0(){
+		Section.resetCounter();
+		Section sec = new Section(new ArrayList<Track>());
+
+		Straight st = new Straight(null,null,sec,100);
+		Straight.StraightRing route = new Straight.StraightRing(st);
+		Track head = route.ringTrack(8, 100);
+		sec.add(head);
+		Track tp_1 = head.getNext(false);
+		Track tp_2 = tp_1.getNext(false);
+		Track tp_3 = tp_2.getNext(false);
+		Track tp_4 = tp_3.getNext(false);
+		Track tp_5 = tp_4.getNext(false);
+		Track tp_6 = tp_5.getNext(false);
+		Track tp_7 = tp_6.getNext(false);
+		Track tp_8 = tp_7.getNext(false);
+
+		//set up the train track.
+
+		// add a locomotive
+
+		Movable locomotive = new Locomotive(new Track[]{head,head},40,40,10,false);
+		Movable.GenerateID.generateID(locomotive);
+
+		Map<Integer,modelrailway.simulation.Train> trainMap = new HashMap<Integer,modelrailway.simulation.Train>();
+		Train train = new Train(new Movable[]{locomotive});
+
+		trainMap.put(0,train );
+		train.setID(0);
+
+		Map<Integer,modelrailway.core.Train> orientationMap = new HashMap<Integer,modelrailway.core.Train>();
+
+		orientationMap.put(0, new modelrailway.core.Train(0, true));
+
+		String portname = "port";
+		Event.Listener sim = null;
+		try {
+			sim = new  modelrailway.ModelRailway(portname, 0);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			fail("initialization error: ");
+		}
+		final TestController ctl = new TestController(orientationMap,route.getSectionMap(),head, sim); //
+
+
+		Integer start = tp_1.getSection().getNumber();
+		Integer s2 = tp_2.getSection().getNumber();
+		Integer s3 = tp_3.getSection().getNumber();
+		Integer s4 = tp_4.getSection().getNumber();
+		Integer s5 = tp_5.getSection().getNumber();
+		Integer s6 = tp_6.getSection().getNumber();
+		Integer s7 = tp_7.getSection().getNumber();
+		Integer s8 = tp_8.getSection().getNumber();
+
+		Route routePlan = new Route(true,  start, s2,s3,s4,s5,s6,s7,s8);
+		//System.out.println("route: "+headSection+", "+switchSection+", "+swAlt+", "+sw2Section);
+
+		final ArrayList<Integer> outputArray = new ArrayList<Integer>();
+		final Thread th = Thread.currentThread();
+		ctl.register(new Listener(){
+			public void notify(Event e){
+				//System.out.println("event "+e.toString());
+				if(e instanceof Event.SectionChanged && ((SectionChanged) e).getInto()){
+				  outputArray.add(((Event.SectionChanged) e).getSection());
+
+				  if(((Event.SectionChanged)e).getSection() == 0){
+
+					  ctl.stop(0);
+					  th.interrupt();
+
+				  }
+				}
+			}
+
+		});
+
+		ctl.start(0, routePlan);
+
+		try{
+			//System.out.println("started: ");
+		   Thread.currentThread().join();
+		 //  System.out.println("stopped:");
+		}catch(InterruptedException e){
+			System.out.println("ctlTest0:");
+			System.out.println(routePlan.toString());
+			System.out.println("output: "+outputArray.toString());
+		}
+
+	}
+}
